12a1249a39f958a9a9e870040e46e9526f1dc38b
Tami Churns <tchurns@paperlesspost.com>
2019-04-03 23:36:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ function fetchCurrentWeather(city) {
 
 function displayCurrentWeather(json) {
   //render current weather data to the DOM using provided IDs and json from API
+  console.log(main.temp) 
 }
 
 
