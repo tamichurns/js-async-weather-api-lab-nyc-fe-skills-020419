9cbdcc737603e4a651ab39d9cd2e2241fc065e72
Tami Churns <tchurns@paperlesspost.com>
2019-04-03 23:30:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function handleFormSubmit(event) {
 function fetchCurrentWeather(city) {
   //fetch current weather based on city
   fetch('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + API_KEY)
-  .then((data) => {return data.json})
+  .then( (data) => {return data.json()} )
   .then( (json) => {console.log(json)} )
   
 }
