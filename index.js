c3632d2fe63a7fc1a403e77134c5311304c2e867
Tami Churns <tchurns@paperlesspost.com>
2019-04-04 00:04:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,7 +53,11 @@ function fetchFiveDayForecast(city) {
 
 function displayFiveDayForecast(json) {
   //render five day forecast data to the DOM using provided IDs and json from API
-  console.log(json)
+  console.log(json) //we have 39 objects in an array and we want to make a div for each 
+  for (let forecast of forecasts) {
+    
+    
+  }
 }
 
 function createChart(json) {
